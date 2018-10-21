6f30083dbbeb21d3764a672dee62e1f47e933c90
Julien DOCHE <julien.doche@live.fr>
2018-10-21 12:52:51 +0200

    Add TODOs
    Signed-off-by: Julien DOCHE <julien.doche@live.fr>

--- roles/install/templates/install.sh
+++ roles/install/templates/install.sh
@@ -7,21 +7,21 @@ ln -sf /usr/share/zoneinfo/{{ timezone.region }}/{{ timezone.city }} /etc/localt
 hwclock --systohc
 
 # Localization
-echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen #TODO: Move to ansible
 locale-gen
-echo "LANG=en_US.UTF-8" > /etc/locale.conf
+echo "LANG=en_US.UTF-8" > /etc/locale.conf #TODO: Move to ansible
 
 # Network configuration
-echo "{{ ansible_hostname }}" > /etc/hostname
-#hosts
+echo "{{ ansible_hostname }}" > /etc/hostname #XXX: Working ? #TODO: Move to ansible
+#hosts #TODO: Move to ansible
 
 # Initramfs
 mkinitcpio -p linux
 
 # Root password
-usermod --password {{ user.password }} root
+usermod --password {{ user.password }} root #TODO: make it work
 
 # Bootloader
 bootctl --path=/boot install
-# timeout
-# entry
+# timeout #TODO: Move to ansible
+# entry #TODO: Move to ansible
